2898c798d016a472fc09720ad62b87f641c2dd06
christofvermeersch <christof.vermeersch@esat.kuleuven.be>
2024-12-19 15:28:00 +0100

    Implemented the code to generate the numerical coefficient matrices

--- Scripts/Applications/h2sisored3.m
+++ Scripts/Applications/h2sisored3.m
@@ -18,5 +18,70 @@ function [mat,supp] = h2sisored3(an,bn,r)
     % Optimal SISO H2-norm Model Reduction Using Walsh's Theorem, IEEE
     % Control System Letters, vol. 7, p. 1970-1675.
 
-    error("Problem is not yet implemented.")
+    % Define the (symbolic) variables:
+    n = length(an);
+    ar = sym("ar",[r 1]);
+    for i = 1:r
+        assume(ar(i),"real")
+    end
+
+    % Create the auxiliary matrices:
+    t = [1 ar' zeros(1,n-1)];
+    Ta = sym('Ta',[n,n+r]);
+    for i = 1:n
+        Ta(i,:) = circshift(t,i-1);
+    end
+    t = [1 an' zeros(1,r-1)];
+    Tb = zeros(r,n+r);
+    for i = 1:r
+        Tb(i,:) = circshift(t,i-1);
+    end
+    t = [1 ar' zeros(1,n-1)];
+    Tc = sym('Tc',[n,n+r]);
+    for i = 1:n
+        Tc(i,:) = circshift(t,i-1);
+    end
+    Td = Tc(1:n-r,1:n);
+
+    % Construct matrix polynomial:
+    A = [Ta.'*bn Tb.' Tc.'*Td.'];
+
+    % Derive the numerical coefficient matrices:
+    [monomials,supp] = powers(ar);
+    mat = cell(length(monomials),1);
+    for i = 1:length(monomials)
+        mat{i} = zeros(size(A));
+    end
+    for i = 1:size(A,1)
+        for j = 1:size(A,2)
+            [coefs,mons] = coeffs(A(i,j));
+            for k = 1:length(mons)
+                idx = find(mons(k) == monomials);
+                temp = mat{idx};
+                temp(i,j) = coefs(k);
+                mat{idx} = temp;
+            end
+        end
+    end
+end
+
+% Define the required subfunction:
+function [monomials,supp] = powers(a)
+    %POWERS   Monomials and support.
+    %   [monomials,supp] = POWERS(a) returns the symbolic monomials and the
+    %   (numerical) support for the different powers of the variables in a.
+    %   All monomials of total degree up to two are required by the main
+    %   function.
+
+    n = length(a);
+    I = fliplr(eye(n,n));
+    supp = zeros(nchoosek(n+2,2),n);
+    supp(2:n+1,:) = I;
+    blockLength = n:-1:1;
+    blockPos = [0; cumsum(blockLength')];
+    for i = n:-1:1
+        supp(n+blockPos(i)+2:n+blockPos(i+1)+1,:) = ...
+            supp(n-blockLength(i)+2:n+1,:) + I(i,:);
+    end
+    monomials = prod(a'.^supp,2);
 end
