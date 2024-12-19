d125b71e92a9ff52cb50c4cabb13cd34ae2ce402
christofvermeersch <christof.vermeersch@esat.kuleuven.be>
2024-12-19 13:37:36 +0100

    Implemented different application scripts

--- Scripts/Applications/h2sisored2.m
+++ Scripts/Applications/h2sisored2.m
@@ -1,5 +1,42 @@
-function mep = h2sisored2(a,b,r)
-    % TODO: add code that mimics Alsubaie's results.
+function [mat,supp] = h2sisored2(an,bn,r)
+    %H2SISORED2   H2-norm model order reduction problem.
+    %   mat = H2SISORED2(an,bn,r) creates the coefficient matrices of the
+    %   multiparameter eigenvalue problem that solves the H2-norm model 
+    %   order reduction problem of an n-th order model given by its 
+    %   coefficients of the transfer function (bn/an) to an r-th order 
+    %   model. It uses the so-called "second approach" from [1].
+    %
+    %   [mat,supp] = H2SISORED2(...) also gives the support of the 
+    %   coefficient matrices.
+        
+    % Copyright (c) 2024 - Christof Vermeersch
+    %
+    % This function implements the construction of the numerical
+    % coefficient matrices as described in [1].
+    %
+    % Note: this approach results in the interpolation points of the 
+    % reduced order model. You find more information about them in [1].
+    %
+    % [1] Alsubaie, F. F. F. (2019), H2 Optimal Model Reduction for Linear
+    % Dynamic Systems and the Solution of Multiparameter Matrix Pencil 
+    % Problems, PhD thesis, Imperial College London, London, UK.
 
-    % Check if Sibren has implemented this code already!
+    % Determine the degree:
+    n = length(an);
+
+    % Create the auxiliary matrices:
+    A = [-an.'; eye(r-1) zeros(r-1,1)];
+    B = zeros(r,1); B(1) = 1;
+    C = bn.';
+
+    % Derive the (numerical) coefficient matrices:
+    mat = cell(r+1,1);
+    mat{1} = [zeros(n,n) (-A)^r A^r*B; (-A)^r -eye(n) zeros(n,1); ...
+        C zeros(r,n+1)];
+    for i = 1:r
+        temp = [zeros(n,n) (-A)^(r-i) A^(r-i)*B; (-A)^(r-i) ...
+            zeros(n,n+1); zeros(m,2*n+1)];
+        mat{i+1} = temp;
+    end
+    supp = [zeros(1,r); eye(r)];
 end
--- Scripts/Applications/h2sisored3.m
+++ Scripts/Applications/h2sisored3.m
@@ -1,5 +1,22 @@
-function mep = h2sisored3(a,b,r)
-    % TODO: add code that mimics Sibren's Walsh results.
+function [mat,supp] = h2sisored3(an,bn,r)
+    %H2SISORED3   H2-norm model order reduction problem.
+    %   mat = H2SISORED3(an,bn,r) creates the coefficient matrices of the
+    %   multiparameter eigenvalue problem that solves the H2-norm model 
+    %   order reduction problem of an n-th order model given by its 
+    %   coefficients of the transfer function (bn/an) to an r-th order 
+    %   model. It uses the so-called "third (Walsh) approach" from [1].
+    %
+    %   [mat,supp] = H2SISORED3(...) also gives the support of the 
+    %   coefficient matrices.
+        
+    % Copyright (c) 2024 - Christof Vermeersch
+    %
+    % This function implements the construction of the numerical
+    % coefficient matrices as described in [1].
+    %
+    % [1] Lagauw, S., Agudelo, O. M., and De Moor, B. (2023) Globally
+    % Optimal SISO H2-norm Model Reduction Using Walsh's Theorem, IEEE
+    % Control System Letters, vol. 7, p. 1970-1675.
 
-    % Check if Sibren has implemented this code already!
+    error("Problem is not yet implemented.")
 end
